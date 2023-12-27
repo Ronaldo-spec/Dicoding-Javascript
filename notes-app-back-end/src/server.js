8d99f545c69a858f2761f56d505f46c9bad890b6
Ronaldo-spec <ronaldofirmansyah.30@gmail.com>
2023-12-27 10:16:59 +0700

    inter-1

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -1,21 +1,30 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
- 
+const Hapi = require("@hapi/hapi");
+//const routes = require("./routes");
+const notes = require("./api/notes");
+const NotesService = require("./services/inMemory/NotesService");
+
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-        routes: {
-          cors: {
-            origin: ['*'],
-          },
-        },
-      });
- 
-server.route(routes);
- 
-await server.start();
-console.log(`Server berjalan pada ${server.info.uri}`);
+  const notesService = new NotesService();
+
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
